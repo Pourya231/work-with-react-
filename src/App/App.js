03368817be3c528a645abafcbe3c3b404269cc4a
pourya <pourya@LAPTOP-FKIRP1VN>
2024-08-01 10:37:56 +0330

    use css in easiest way

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import Button from '../Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        this is sample 
+        </h1> 
+        <p  className='paragraph1'>
+          this is a sample paragraph for test 
+        </p>
+        <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button/Button.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import './Button.css'
+const Button = () => (<button className='button1'> click here</button> )
+
+export default Button 
